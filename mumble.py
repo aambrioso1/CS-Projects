88b21c85c5689d29c0803701d11f71c8673faec0
Alex Ambrioso <aambrioso1@gmail.com>
2019-08-28 23:13:28 -0400

    Add files via upload

--- mumble.py
+++ mumble.py
@@ -1,5 +1,3 @@
-# Mumble Mumble Input Parser
-
 def word_parse(text):
     'Breaks up text into words defined as characters separted by a space'
     low = 0  # Position of the beginning of a word
@@ -13,9 +11,8 @@ def word_parse(text):
     word_list.append(text[low:len(text)]) # Put the last word on the list
     return word_list
 
-list1 = '1 mumble 3 mumble 5 mumble mumble 7 8'
+list1 = '1 mumble 3 mumble 5 mumble mumble 8 mumble'
 
 words = word_parse(list1)
 print('Here is the list of words spoken by Vesa: {}.'.format(words))
 print('Vesa tried to count to {}.'.format(len(words)))
-
